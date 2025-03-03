6f8e4d40e6e4cab7bcbc4fead05ad8db461fe733
pedku <elpekuluis@gmail.com>
2025-03-03 15:34:59 -0500

    Agregar migraciones de base de datos al inicializar la aplicación

--- init_db.py
+++ init_db.py
@@ -1,5 +1,7 @@
 from qr_access_system.app import app, db, create_initial_user
+from flask_migrate import upgrade
 
 with app.app_context():
     db.create_all()
     create_initial_user()
+    upgrade()
